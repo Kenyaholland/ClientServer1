ed05f0c89a0e6cca6c09426cecffd163d9ca2cb0
Kenyaholland <kenyamholland@gmail.com>
2021-02-26 14:46:50 -0600

    Succesfully connected to web browser

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,49 @@
  * @param argv pointer to arguments
  * @return int 0 if everything is ok
  */
+
+#include <stdio.h>          //Standard library
+#include <stdlib.h>         //Standard library
+#include <sys/socket.h>     //API and definitions for the sockets
+#include <sys/types.h>      //more definitions
+#include <netinet/in.h>     //Structures to store address information
+#include <unistd.h>         //close function
+#include <string.h>         //strlen
+
 int main(int argc, char const *argv[])
 {
+
+    int port = atoi(argv[1]);
+    char* message = (char*)argv[2];
+
+    char tcp_server_message[256] = "HTTP/1.1 200 Okay\r\nContent-Type: text/html; charset=ISO-8859-4 \r\n\r\n<title>Highly sensitive data</title></head><body><p> Hello, I am the TCP Server you successfully connected to!! \n\n               Bye Bye!!!\n\n</p></body></html>";
+
+    //create the server socket
+    int tcp_server_socket;                                  //variable for the socket descriptor
+    tcp_server_socket = socket(AF_INET, SOCK_STREAM, 0);    //calling the socket function. Params: Domain of the socket (Internet in this case), type of socket stream (TCP), Protocol (default, 0 for TCP)
+
+    //define the server address
+    struct sockaddr_in tcp_server_address;                  //declaring a structure for the address
+    tcp_server_address.sin_family = AF_INET;                //Structure Fields' definition: Sets the address family of the address the client would connect to
+    tcp_server_address.sin_port = htons(port);             //Passing the port number - converting in right network byte order
+    tcp_server_address.sin_addr.s_addr = INADDR_ANY;        //Connecting to 0.0.0.0
+
+    // binding the socket to the IP address and port
+    bind(tcp_server_socket, (struct sockaddr *) &tcp_server_address, sizeof(tcp_server_address));  //Params: which socket, cast for server address, its size
+
+    //listen for simultaneous connections
+    listen(tcp_server_socket, 5);  //which socket, how many connections
+
+    int tcp_client_socket;
+    tcp_client_socket = accept(tcp_server_socket, NULL, NULL);  // server socket to interact with client, structure like before - if you know - else NULL for local connection
+
+    //send data stream
+    send(tcp_client_socket, tcp_server_message, strlen(tcp_server_message), 0);  // send where, what, how much, flags (optional)
+
+    //close the socket
+    close(tcp_server_socket);
+
     return 0;
 }
+
+
